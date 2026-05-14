254b2318c18ae51aadd79822ef0ffe35bb2b1178
Tushar Damahe <qubetushar@gmail.com>
2026-05-14 14:51:45 +0530

    Configure production CORS

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ const app = express();
 
 await connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Server is running"));
